98646fe412d82b05c8e958ad01d55896c7d21974
Harshvardhanp4 <harshupatil.224@gmail.com>
2025-02-18 20:10:34 +0530

    updates

--- week_2/http-server-2/index.js
+++ week_2/http-server-2/index.js
@@ -1,22 +1,22 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000
-
+app.use(bodyParser.json());
 app.get('/', (req, res) => {
-    
+    console.log(req.body);
     res.send('<b>Yo buddy!</b>')
 })
 
-app.post('/conversations', (req, res) => {
-
-     console.log(req.headers["authorization"])   
+app.post('/', (req, res) => {
+    console.log(req.body);
 
     res.send({
-        msg: "2 + 2 = 4"
+        msg: "2 + 2 =
     }
     )
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+}) 
